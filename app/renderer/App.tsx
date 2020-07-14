0e60382087a7fd9d6eeb4ca3d2b21a65ef268842
Doroshev Yerkebulan <erke01vip@gmail.com>
2020-07-14 23:20:03 +0600

    fix: minor fix of fix

--- app/renderer/App.tsx
+++ app/renderer/App.tsx
@@ -1,5 +1,5 @@
 var React = require("react");
-import { Heading, Example } from "@components";
+import { Heading, Example } from "./components";
 import { hot as App } from "react-hot-loader/root";
 
 export default App(
