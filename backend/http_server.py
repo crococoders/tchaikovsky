fcf8aa03e8c9e569fb13e72af820d1fe637b9c65
zhanybekovv <nartay.zhanybekov@nu.edu.kz>
2020-07-14 15:12:31 +0600

    chore: add info on auth
    feat: add token authentication
    
    feat: add authentication token

--- backend/http_server.py
+++ backend/http_server.py
@@ -1,14 +1,14 @@
 import json
 import jsonpickle
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
 from yandex_music.client import Client
 import logging
 logging.basicConfig(level=logging.CRITICAL)
 
 
-
 class S(BaseHTTPRequestHandler):
-    data = {}
+    token = ""
 
     def _set_response(self, code):
         self.send_response(code)
@@ -16,8 +16,10 @@ class S(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
+        # token = "AgAAAAAcPiuVAAG8XhlKrZWp1E1SrUv2dHA-q5Y"
+        client = Client.from_token(token)
         if self.path == '/liked_tracks/':
-            client = Client.from_credentials(data['login'], data['password'])# <--- client object, accessing yandex account
+            print("token", token)
             liked_tracks = client.users_likes_tracks().tracks
             print("signed up as ", client.me['account']['full_name'])
             response = {'result': []}
@@ -26,21 +28,35 @@ class S(BaseHTTPRequestHandler):
             json_dump = jsonpickle.encode(response)
             self._set_response(200)
             self.wfile.write(json_dump.encode("utf-8"))
+        elif self.path == '/play/':
+            title = client.users_likes_tracks().tracks[0].track.title
+            print(client.users_likes_tracks().tracks[0].track.file_size)
+            client.users_likes_tracks().tracks[0].track.download(title)
+            self._set_response(200)
+            # print(track)
+        else:
+            self._set_response(404)
+            self.wfile.write("Not found".encode('utf-8'))
+
+    def do_POST(self):
+        if self.path == '/auth/':
+            global token
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length).decode("utf-8")  # <--- Reads data from body
+            data = json.loads(post_data)  # <--- Converts string to json
+            print("data", data)
+            client1 = Client()
+            try:
+                token = client1.generate_token_by_username_and_password(username=data['login'], password=data['password'])
+                self._set_response(200)
+                self.wfile.write("OK".encode('utf-8'))
+            except:
+                self._set_response(403)
+                self.wfile.write("invalid password or login".encode("utf-8"))
         else:
             self._set_response(404)
             self.wfile.write("Error".encode('utf-8'))
 
-    def do_POST(self):
-        if self.path == '/auth/':
-            global data
-            content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-            post_data = self.rfile.read(content_length).decode("utf-8") # <--- Reads data from body
-            data = json.loads(post_data) # <--- Converts string to json
-            self._set_response(200)
-            self.wfile.write("OK".encode('utf-8'))
-        else:
-            self._set_response(404)
-            self.wfile.write("Error".encode('utf-8'))
 
 def run(server_class=HTTPServer, handler_class=S, port=8000):
     server_address = ('', port)
