334df8af8dd71fb921be24092d92b40bac53a406
abdigali1 <abdigali2@gmail.com>
2020-08-03 22:10:11 +0600

    fix: delete .idea file, fix aliases and add usage example of this alias

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,27 +1,27 @@
-const path = require('path');
-const rules = require('./webpack.rules');
+const path = require("path");
+const rules = require("./webpack.rules");
 
 function srcPaths(src) {
   return path.join(__dirname, src);
 }
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  target: 'electron-main',
-  entry: './app/main/main.ts',
+  mode: "development",
+  devtool: "source-map",
+  target: "electron-main",
+  entry: "./app/main/main.ts",
   module: {
-    rules: require('./webpack.rules'),
+    rules: require("./webpack.rules"),
   },
   resolve: {
     alias: {
-      '@main': srcPaths('app/main'),
-      '@models': srcPaths('app/models'),
-      '@renderer': srcPaths('app/renderer'),
-      "@static": srcPaths('./static'),
-      "@styles": srcPaths('./static/styles'),
-      "@components": srcPaths('./app/renderer/components'),
+      "@main": srcPaths("app/main"),
+      "@models": srcPaths("app/models"),
+      "@renderer": srcPaths("app/renderer"),
+      "@static": srcPaths("./static"),
+      "@styles": srcPaths("./static/styles"),
+      "@components": srcPaths("./app/renderer/components"),
     },
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json', '.scss']
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json", ".scss"],
   },
 };
